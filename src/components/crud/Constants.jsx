f810fc1e5c0d2bf66d257935c6fe405e84ce4b84
hyparh <hyparh@gmail.com>
2023-12-03 18:10:34 +0200

    Make more fields for create telescope

---
+++ src/components/crud/Constants.jsx
@@ -0,0 +1,10 @@
+export const telescopeTypes = ["Reflector", "Refractor"];
+export const buildTypes = [
+  "Newton",
+  "Schmidt-Cassegrain",
+  "Achromat",
+  "Apochromat",
+  "Astrograph",
+];
+export const mountingTypes = ["Azimuthal", "Equatorial", "Dobson", "No Mount"];
+export const gotoControls = ["Yes", "No"];
--- src/components/crud/CreateTelescope.jsx
+++ src/components/crud/CreateTelescope.jsx
@@ -10,10 +10,15 @@ import {
   onSnapshot,
   doc,
 } from "firebase/firestore";
+import { telescopeTypes, buildTypes, mountingTypes, gotoControls } from "./Constants";
 
 function CreateTelescope() {
-  const [newBrand, setNewBrand] = useState("");
+  const [newType, setNewType] = useState("");
+  const [newBuildType, setNewBuildType] = useState("");
+  const [newMountingType, setNewMountingType] = useState("");
+  const [newGotoControl, setNewGotoControl] = useState("");
   const [newAperture, setNewAperture] = useState(0);
+  const [newPrice, setNewPrice] = useState(0);
   const [telescopes, setTelescopes] = useState([]);
   const telescopesCollectionRef = collection(db, "telescopes");
 
@@ -23,13 +28,16 @@ function CreateTelescope() {
     if (user) {
       try {
         const newTelescope = {
-          brand: newBrand,
+          type: newType,
+          mountingType: newMountingType,
+          buildType: newBuildType,
+          gotoControl: newGotoControl,
           aperture: Number(newAperture),
+          price: Number(newPrice),
           userId: user.uid,
         };
 
         const docRef = await addDoc(telescopesCollectionRef, newTelescope);
-
       } catch (error) {
         console.error("Error creating telescope:", error.message);
       }
@@ -58,16 +66,76 @@ function CreateTelescope() {
           CREATE NEW
         </label>
         <br></br>
-        <input
-          placeholder="Brand..."
+        <select
+          value={newType}
           onChange={(event) => {
-            setNewBrand(event.target.value);
+            setNewType(event.target.value);
           }}
-        ></input>
+        >
+          <option value="" disabled>
+            Select Type
+          </option>
+          {telescopeTypes.map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+        <select
+          value={newBuildType}
+          onChange={(event) => {
+            setNewBuildType(event.target.value);
+          }}
+        >
+          <option value="" disabled>
+            Select Build Type
+          </option>
+          {buildTypes.map((buildType) => (
+            <option key={buildType} value={buildType}>
+              {buildType}
+            </option>
+          ))}
+        </select>
+        <select
+          value={newMountingType}
+          onChange={(event) => {
+            setNewMountingType(event.target.value);
+          }}
+        >
+          <option value="" disabled>
+            Select Mounting Type
+          </option>
+          {mountingTypes.map((mountingType) => (
+            <option key={mountingType} value={mountingType}>
+              {mountingType}
+            </option>
+          ))}
+        </select>
+        <select
+          value={newGotoControl}
+          onChange={(event) => {
+            setNewGotoControl(event.target.value);
+          }}
+        >
+          <option value="" disabled>
+            Select Goto Control
+          </option>
+          {gotoControls.map((gotoControl) => (
+            <option key={gotoControl} value={gotoControl}>
+              {gotoControl}
+            </option>
+          ))}
+        </select>
         <input
           placeholder="Aperture in mm..."
           onChange={(event) => {
             setNewAperture(event.target.value);
+          }}
+        ></input>
+        <input
+          placeholder="Price..."
+          onChange={(event) => {
+            setNewPrice(event.target.value);
           }}
         ></input>
         <br></br>
@@ -77,8 +145,12 @@ function CreateTelescope() {
         {telescopes.map((telescope) => {
           return (
             <div key={telescope.id}>
-              <h1>Brand: {telescope.brand}</h1>
+              <h1>Type: {telescope.type}</h1>
+              <h1>Build type: {telescope.buildType}</h1>
               <h1>Aperture: {telescope.aperture}</h1>
+              <h1>Mounting type: {telescope.mountingType}</h1>
+              <h1>Goto control: {telescope.gotoControl}</h1>
+              <h1>Price: {telescope.price}</h1>
               <button
                 onClick={() => {
                   updateTelescope(telescope.id, telescope.aperture);
