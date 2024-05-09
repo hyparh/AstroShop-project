962a66421e61809c6dce56849c8d7071e294b1e6
hyparh <hyparh@gmail.com>
2024-05-09 17:40:09 +0300

    Make brand icons lead to website when clicked

--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -5,13 +5,13 @@ export default function Footer() {
     <footer className="footer-form">
       <div className="footer-container">
         <p>&copy; Astro Shop {currentYear}</p>
-        <img src="/logos/1.png" alt="" />
-        <img src="/logos/2.jpg" alt="" />
-        <img src="/logos/3.jpg" alt="" />
-        <img src="/logos/4.png" alt="" />
-        <img src="/logos/5.jpg" alt="" />
-        <img src="/logos/6.jpg" alt="" />
-        <img src="/logos/7.png" alt="" />
+        <a href="https://www.celestron.com/" target="_blank"><img src="/logos/1.png" alt="" /></a>
+        <a href="https://global.vixen.co.jp/en/" target="_blank"><img src="/logos/2.jpg" alt="" /></a>
+        <a href="https://skywatcher.com/" target="_blank"><img src="/logos/3.jpg" alt="" /></a>
+        <a href="https://www.orionoptics.co.uk/" target="_blank"><img src="/logos/4.png" alt="" /></a>
+        <a href="https://www.bresser.de/en/Brand/Bresser/" target="_blank"><img src="/logos/5.jpg" alt="" /></a>
+        <a href="https://www.omegon.eu/" target="_blank"><img src="/logos/6.jpg" alt="" /></a>
+        <a href="https://levenhuk.com/" target="_blank"><img src="/logos/7.png" alt="" /></a>
       </div>
     </footer>
   );
